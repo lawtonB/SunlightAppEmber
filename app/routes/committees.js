97eca4d7f14c7012fffa0e1fde346bf67cac83f8
david diehr and lawton browning <david+lawton@>
2016-04-05 11:39:39 -0700

    display subcommittee

--- app/routes/committees.js
+++ app/routes/committees.js
@@ -2,8 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    var url='http://congress.api.sunlightfoundation.com/committees?chamber=house&subcommittee=false&apikey=6fa70caf0ff646f88ce164da51fa6f33';
-    return Ember.$.getJSON(url).then(function(responseJSON){
+    var urlHouse='http://congress.api.sunlightfoundation.com/committees?chamber=house&subcommittee=false&apikey=6fa70caf0ff646f88ce164da51fa6f33';
+    return Ember.$.getJSON(urlHouse).then(function(responseJSON){
+      // console.log(JSON.stringify(responseJSON));
       return responseJSON.results;
     });
   }
--- app/routes/results.js
+++ app/routes/results.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
   model: function(params) {
     var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=6fa70caf0ff646f88ce164da51fa6f33&zip=' + params.zip;
     return Ember.$.getJSON(url).then(function(responseJSON){
-      console.log(JSON.stringify(responseJSON));
+      // console.log(JSON.stringify(responseJSON));
       return responseJSON.results;
     });
   }
